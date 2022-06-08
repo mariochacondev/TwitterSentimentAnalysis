7e83218cfeda9426fd3ff6d42a1b3afce5893312
mario <mariojosech@gmail.com>
2022-06-08 15:30:04 +0200

    Functions Refactoring 2

--- mainTw.py
+++ mainTw.py
@@ -2,52 +2,51 @@ import re
 from textblob import TextBlob
 import tweepy
 from login import *
-import pandas as pd
 import emoji
+from pydantic import BaseModel
 
-pd.set_option('max_colwidth', 30)
-pd.set_option('display.max_columns', 4)
-pd.set_option('display.max_rows', None)
 
-tweets_list = []
-formatted_tweets = []
+class Tweet(BaseModel):
+    username: str
+    created_at: str | None = ''
+    tweet: str
+    subjectivity: str | None = ''
+    sentiment: str | None = ''
 
-def auth_twitter():
+
+
+def twitter_client():
     auth = tweepy.OAuthHandler(APY_KEY, API_SECRET)
     auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-    client = tweepy.API(auth)
-    return client
+    return tweepy.API(auth)
 
 
-def get_tweets(keyword):
-    client = auth_twitter()
-    limit = 150
-    tweets = tweepy.Cursor(client.search_tweets,
-                           q=keyword,
-                           count=limit,
-                           tweet_mode='extended').items(limit)
-    for tweet in tweets:
-        each_tweet = {'Username': tweet.user.screen_name, 'Tweet': tweet.full_text}
-        tweets_list.append(each_tweet)
-    return tweets_list
+def get_tweets(keyword, client, limit):
+    tweets = []
+    response = tweepy.Cursor(client.search_tweets,
+                             q=keyword+'-filter:retweets',
+                             count=limit,
+                             tweet_mode='extended').items(limit)
+    for tweet in response:
+        each_tweet = Tweet(username=tweet.user.screen_name, tweet=tweet.full_text, created_at=tweet.created_at.strftime("%d/%m/%Y %H:%M"))
+        tweets.append(each_tweet)
+    return tweets
 
 
-def clean_tweets():
-    for tweet in tweets_list:
-        tweet['Tweet'] = re.sub('\\n', '', tweet['Tweet'])
-        tweet['Tweet'] = emoji.replace_emoji(tweet['Tweet'], replace='')
-        tweet['Tweet'] = re.sub('#[A-Za-z0-9]+', '', tweet['Tweet'])
-        tweet['Tweet'] = re.sub(u'[\u4e00-\u9fff]', '', tweet['Tweet'])
-        tweet['Tweet'] = re.sub(r"http\S+", '', tweet['Tweet'])
-        formatted_tweets.append(tweet)
+def process_tweets(tweets_list):
+    formatted_tweets = []
+    for elem in tweets_list:
+        elem.tweet = emoji.replace_emoji(elem.tweet, replace='')
+        elem.tweet = ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", elem.tweet).split())
+        if elem.tweet:
+            formatted_tweets.append(elem)
     return formatted_tweets
 
 
-def analyze_tweets():
-    df = pd.DataFrame()
-    for tweet in formatted_tweets:
-        tweet_subjectivity = TextBlob(tweet['Tweet']).sentiment.subjectivity
-        tweet_polarity = TextBlob(tweet['Tweet']).sentiment.polarity
+def analyze_tweets(formatted_tweets):
+    for elem in formatted_tweets:
+        tweet_subjectivity = TextBlob(elem.tweet).sentiment.subjectivity
+        tweet_polarity = TextBlob(elem.tweet).sentiment.polarity
         if tweet_polarity < 0:
             polarity = 'Negative'
         elif tweet_polarity == 0:
@@ -61,17 +60,17 @@ def analyze_tweets():
             subjectivity = 'Subjective'
         else:
             subjectivity = 'Very Subjective'
-        data = pd.DataFrame({'Username': tweet['Username'],
-                             'Tweet': [tweet['Tweet']],
-                             'Subjectivity': [subjectivity],
-                             'Sentiment': [polarity]})
-        df = pd.concat([df, data], ignore_index=True)
-    return df.set_index('Username')
+
+        elem.subjectivity = subjectivity
+        elem.sentiment = polarity
+    return formatted_tweets
 
 
 if __name__ == '__main__':
     keyword = input('Enter the keyword to search and analyse: ')
-    get_tweets(keyword)
-    clean_tweets()
-    print(analyze_tweets())
+    client = twitter_client()
+    tweets = get_tweets(keyword, client, limit=50)
+    cleaned_tweets = process_tweets(tweets)
+    for elem in analyze_tweets(cleaned_tweets):
+        print(elem)
 
